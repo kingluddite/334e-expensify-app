f494d9a766df0cd48ed5dbd64bee5b776d938f9e
Kingluddite <howley.phil@gmail.com>
2019-11-25 21:22:27 -0800

    Remove like to test firebase

--- src/app.js
+++ src/app.js
@@ -15,8 +15,6 @@ import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import AppRouter from './routers/AppRouter';
 import './styles/styles.scss';
-// temp import firbase for testing
-import './firebase/firebase';
 
 const store = configureStore();
 
