2202783d3633ca5248daeeea09bf2b88b627f4c1
Kingluddite <howley.phil@gmail.com>
2019-11-29 12:15:32 -0800

    add prod webpack

---
+++ webpack.config.prod.js
@@ -0,0 +1,56 @@
+const path = require('path'); // to get the current path
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const fs = require('fs');
+
+const prodMode = process.env.NODE_ENV === 'production';
+
+module.exports = env => {
+  // console.log('env', env);
+  // const isProduction = env === 'production';
+  console.log(process.env.ENVIRONMENT);
+  const CSSExtract = new MiniCssExtractPlugin({
+    filename: '[name].[hash].css',
+    chunkFilename: '[id].[hash].css',
+  });
+
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public', 'dist'),
+      filename: 'bundle.js',
+    },
+    optimization: {
+      minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(scss|css)$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+            },
+            { loader: 'css-loader', options: { sourceMap: true } },
+            { loader: 'sass-loader', options: { sourceMap: true } },
+          ],
+        },
+      ],
+    },
+    plugins: [CSSExtract],
+    devtool: 'source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+      publicPath: '/dist/',
+    },
+  };
+};
