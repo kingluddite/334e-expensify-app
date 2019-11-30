f1e31ac1c0e76fd87762d8fb79478a7b1db0387f
Kingluddite <howley.phil@gmail.com>
2019-11-30 10:41:14 -0800

    Setup test db environment

--- webpack.config.js
+++ webpack.config.js
@@ -6,33 +6,44 @@ const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const fs = require('fs');
 
+// what environment are we in?
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+if (process.env.NODE_ENV === 'test') {
+  // set up test environment variables
+  require('dotenv').config({ path: '.env.test' });
+} else if (process.env.NODE_ENV === 'development') {
+  // set up development environment variables
+  require('dotenv').config({ path: '.env.development' });
+}
+
 // to check if the file exists
 const devMode = process.env.NODE_ENV !== 'production';
-
+// console.log(devMode);
 module.exports = env => {
   // Get the root path (assuming your webpack config is in the root of your project)
-  const currentPath = path.join(__dirname);
+  // const currentPath = path.join(__dirname);
 
   // Create the fallback path (the production .env)
-  const basePath = `${currentPath}/.env`;
+  // const basePath = `${currentPath}/.env`;
 
   // We're concatenating the environment name to our filename to specify the correct .env file
-  const envPath = `${basePath}.${env.ENVIRONMENT}`;
+  // const envPath = `${basePath}.${env.ENVIRONMENT}`;
 
   // Check if the file exists, otherwise fall back to the production .env
-  const finalPath = fs.existsSync(envPath) ? envPath : basePath;
+  // const finalPath = fs.existsSync(envPath) ? envPath : basePath;
 
   // Set the path parameter in the dotenv config
-  const fileEnv = dotenv.config({ path: finalPath }).parsed;
+  // const fileEnv = dotenv.config({ path: finalPath }).parsed;
 
   // call dotenv and it will return an Object with a parsed key
   // const env = dotenv.config().parsed;
 
   // reduce it to a nice object with the variables from the file
-  const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
-    return prev;
-  }, {});
+  // const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
+  //   prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
+  //   return prev;
+  // }, {});
 
   // console.log('env', env);
   // const isProduction = env === 'production';
@@ -76,7 +87,7 @@ module.exports = env => {
     },
     plugins: [
       CSSExtract,
-      new webpack.DefinePlugin(envKeys),
+      // new webpack.DefinePlugin(envKeys),
       new webpack.DefinePlugin({
         'process.env.FIREBASE_API_KEY': JSON.stringify(
           process.env.FIREBASE_API_KEY
